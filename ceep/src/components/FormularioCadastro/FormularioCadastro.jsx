ecc188cb84bd096be72116333c4a809bf047037e
thaisascastro <thaisascastro@gmail.com>
2021-07-11 16:30:38 -0300

    passando propriedades

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,44 +2,50 @@ import React, { Component } from "react";
 import "./style.css";
 export default class FormularioCadastro extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.titulo = "";
     this.texto = "";
   }
 
-  handleMudancaTitulo(event) {
+  _handleMudancaTitulo(event) {
     this.titulo = event.target.value
+    event.stopPropagation();
   }
 
-  handleMudancaTexto(event) {
+  _handleMudancaTexto(event) {
     this.texto = event.target.value
+    event.stopPropagation();
   }
 
-  criarNota(event) {
-    console.log(`uma nova nota foi criada {this.titulo} e {this.texto}.`)
+  _criarNota(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.props.criarNota(this.titulo, this.texto)
   }
 
   render() {
     return (
       <form className="form-cadastro"
-      onSubmit= {this.criarNota.bind(this)}
+      onSubmit= {this._criarNota.bind(this)}
       >
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
-          onChange={this.handleMudancaTitulo.bind(this)}
+          onChange={this._handleMudancaTitulo.bind(this)}
           // chamar função quando for desparado handler
           // {} significa evento JS
           // constructor -> guardar atributos ao clicar
           // _ -> significa privados a instância
+          // preventDefault -> prevenir o evento sem carregar página
+          // stopPropagation -> impedir que desencadeie event click
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
-          onChange={this.handleMudancaTexto.bind(this)}
+          onChange={this._handleMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
