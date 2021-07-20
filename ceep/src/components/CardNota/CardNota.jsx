c0b72fb2caa99404b654acff426743bb7a72fb1a
thaisascastro <thaisascastro@gmail.com>
2021-07-20 16:12:54 -0300

    propriedades components

--- ceep/src/components/CardNota/CardNota.jsx
+++ ceep/src/components/CardNota/CardNota.jsx
@@ -5,14 +5,20 @@ import {ReactComponent as DeleteSVG} from "../../assets/delete.svg";
 // para usar o react component usa-se o {ReactComponent as  .... (em maiúsculo))}
 
 export default class CardNota extends Component {
+  apagar(){
+    const indice = this.props.indice;
+    this.props.apagarNota(indice);
+  }
+  
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
-          <DeleteSVG onClick={() => console.log("Deleta")}/>
+          <DeleteSVG onClick={this.apagar.bind(this)}/>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
+
       </section>
     );
   }
