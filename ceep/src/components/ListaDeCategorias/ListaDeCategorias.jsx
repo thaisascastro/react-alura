300f3e1f7f522d61dd37044b11f2ad3105c16f70
thaisascastro <thaisascastro@gmail.com>
2022-01-10 20:39:59 -0300

    classnames

--- ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,18 +1,26 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import './styles.css';
 
 class ListaDeCategorias extends Component {
+
+  _handleEventoInput(e) {
+    console.log(e)
+  }
+
   render() {
     return (
-      <Fragment className="lista-categorias">
+      <section className="lista-categorias">
       <ul className="lista-categorias_lista">
         <li className="lista-categorias_item">Categorias</li>
         <li>Categorias</li>
         <li>Categorias</li>
         <li>Categorias</li>
       </ul>
-      <input type="text" className="lista-categorias_input"/>
-      </Fragment>
+      <input type="text" 
+      className="lista-categorias_input" 
+      placeholder="Adicionar Categoria" 
+      onKeyUp={this._handleEventoInput.bind(this)}/>
+      </section>
     );
   }
 }
