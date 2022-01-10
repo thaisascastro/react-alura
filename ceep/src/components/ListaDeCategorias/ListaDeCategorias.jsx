da03ff9419586626603061cf3b6bd8d21f89edf6
thaisascastro <thaisascastro@gmail.com>
2022-01-10 20:15:20 -0300

    add css em lista de categorias

--- ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,16 +1,17 @@
 import React, { Component, Fragment } from "react";
+import './styles.css';
 
 class ListaDeCategorias extends Component {
   render() {
     return (
-      <Fragment>
-      <ul>
-        <li>Categorias</li>
+      <Fragment className="lista-categorias">
+      <ul className="lista-categorias_lista">
+        <li className="lista-categorias_item">Categorias</li>
         <li>Categorias</li>
         <li>Categorias</li>
         <li>Categorias</li>
       </ul>
-      <input type="text" />
+      <input type="text" className="lista-categorias_input"/>
       </Fragment>
     );
   }
