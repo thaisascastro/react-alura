e6a5b932d97819e8b48aac980827118c2c7c0224
thaisascastro <thaisascastro@gmail.com>
2022-01-11 20:57:03 -0300

    indentando lista de categorias

--- ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -16,15 +16,15 @@ class ListaDeCategorias extends Component {
   render() {
     return (
       <section className="lista-categorias">
-      <ul className="lista-categorias_lista">
+        <ul className="lista-categorias_lista">
         {this.props.categorias.map((categorias, index) => {
           return <li key={index} className="lista-categorias_item">{categorias}</li>
         })}      
-      </ul>
+        </ul>
       <input type="text" 
-      className="lista-categorias_input" 
-      placeholder="Adicionar Categoria" 
-      onKeyUp={this._handleEventoInput.bind(this)}/>
+        className="lista-categorias_input" 
+        placeholder="Adicionar Categoria" 
+        onKeyUp={this._handleEventoInput.bind(this)}/>
       </section>
     );
   }
