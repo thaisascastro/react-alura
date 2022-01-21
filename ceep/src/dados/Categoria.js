52ffd54d7d1ec87ae09dd8f4fea96e6cb40071bb
thaisascastro <thaisascastro@gmail.com>
2022-01-20 21:26:55 -0300

    chamando o evento e deixando objeto privado

--- ceep/src/dados/Categoria.js
+++ ceep/src/dados/Categoria.js
@@ -1,6 +1,19 @@
 export default class Categorias {
   constructor() {
     this.categorias = [];
+    this._inscritos = [];
+
+    // _ significa objeto privado
+  }
+
+  inscrever(func) {
+    this._inscritos.push(func);
+  }
+
+  notificar() {
+    this._inscritos.forEach((func) => {
+      func(this.categorias);
+    });
   }
 
   adicionarCategorias(novaCategoria) {
